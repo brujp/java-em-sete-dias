992598e9030f1d27da6f61fbd1492bfd916035fe
Bruno Pinto <bruno.jpinto@outlook.com>
2023-05-05 18:31:19 -0300

    Comentando o desafio B do Dia 4

--- src/Dia_4/DesafioBDia4.java
+++ src/Dia_4/DesafioBDia4.java
@@ -6,16 +6,22 @@ public class DesafioBDia4 {
 
     public static void main(String[] args) {
 
+        //Pegar uma palavra ou frase e separar cada caracter com um espaço
+
+        //Instânciando o Scanner
         Scanner sc = new Scanner(System.in);
 
+        //Solicitando a palavra
         System.out.println("Escreva uma palavra ou frase: ");
         String palavra = sc.nextLine();
 
+        //Percorrendo a palavra caracter por caracter e imprimindo
         for (int i = 0; i < palavra.length(); i++) {
                 char c = palavra.charAt(i);
                 System.out.print(c + " ");
         }
 
+        //Fechando o Scanner
         sc.close();
     }
 }
