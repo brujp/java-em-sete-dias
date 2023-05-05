4d077ff4eaa1d164068076f8671b0d642b21e04e
Bruno Pinto <bruno.jpinto@outlook.com>
2023-05-05 18:29:16 -0300

    Desafio B - Dia 4

--- src/Dia_4/DesafioBDia4.java
+++ src/Dia_4/DesafioBDia4.java
@@ -1,8 +1,21 @@
 package Dia_4;
 
+import java.util.Scanner;
+
 public class DesafioBDia4 {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escreva uma palavra ou frase: ");
+        String palavra = sc.nextLine();
+
+        for (int i = 0; i < palavra.length(); i++) {
+                char c = palavra.charAt(i);
+                System.out.print(c + " ");
+        }
+
+        sc.close();
     }
 }
