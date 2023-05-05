fd1d863165ffc34b91f40f328737ca535db1423d
Bruno Pinto <bruno.jpinto@outlook.com>
2023-05-05 18:01:32 -0300

    for em array

---
+++ src/Dia_4/ForEmArray.java
@@ -0,0 +1,15 @@
+package Dia_4;
+
+public class ForEmArray {
+
+    public static void main(String[] args) {
+
+        //Criando um array de String
+        String[] carros = {"BMW", "Honda", "Tesla"};
+
+        //Fazendo um loop para percorrer o array criado
+        for (int i = 0; i < carros.length; i++) {
+            System.out.println(carros[i]);
+        }
+    }
+}
