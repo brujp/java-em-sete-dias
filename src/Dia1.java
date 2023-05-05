f395f25b3f8674fb64d976bfcb33d4275f29cbf5
Bruno Pinto <bruno.jpinto@outlook.com>
2023-05-05 09:07:45 -0300

    setup inicial

---
+++ src/Dia1.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Dia1 {
+
+    public static void main(String[] args) {
+
+        //Abrindo o Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Solicitando o primeiro número e armazenando na variável
+        System.out.println("Digite um número: ");
+        double numeroUm = sc.nextDouble();
+
+        //Solicitando o segundo número e armazenando na variável
+        System.out.println("Digite outro número: ");
+        double numeroDois = sc.nextDouble();
+
+        //Criando uma variável para armazenar o resultado da soma entre os dois números
+        double soma = numeroUm + numeroDois;
+
+        //Utilizando o DecimalFormat para formatar o double com uma casa decimal
+        DecimalFormat df = new DecimalFormat("#,###.0");
+
+        //Imprimindo o resultado formatado com uma casa decimal
+        System.out.print("A soma de " + numeroUm + " + " + numeroDois + " é: " + df.format(soma));
+
+        //Fechando o Scanner
+        sc.close();
+    }
+}
---
+++ src/Dia2.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Dia2 {
+
+    public static void main(String[] args) {
+
+        //Criando um Array do tipo int
+        int[] valores = {10, 20, 15, 30, 40};
+
+        //Utilizando o Arrays para imprimir minha lista valores
+        System.out.println(Arrays.toString(valores));
+
+        //Criando um segundo Array do tipo String
+        String[] carros = {"Jeep", "Honda", "Fiat", "Ferrari"};
+
+        //Imprimindo o primeiro valor do Array carros (Jeep)
+        System.out.println(carros[0]);
+
+        //Atribuindo outro valor ao primeiro index do Array carros (Jeep = Hyundai)
+        carros[0] = "Hyundai";
+        System.out.println(Arrays.toString(carros));
+
+        //Utilizando o Arrays para imprimir minha lista carros
+        System.out.println(Arrays.toString(carros));
+
+        //Ordenando e imprimindo as listas
+        Arrays.sort(valores);
+        System.out.println(Arrays.toString(valores));
+
+        Arrays.sort(carros);
+        System.out.println(Arrays.toString(carros));
+
+        //Imprimindo o tamanho dos Arrays
+        System.out.println(valores.length);
+        System.out.println(carros.length);
+
+        //Arrays multidimensionais - Array com linhas e colunas
+        String[][] carrosMultidimensional = {{"Marca", "BMW"},{"Marca", "Honda"}};
+        System.out.println(Arrays.deepToString(carrosMultidimensional));
+
+    }
+}
